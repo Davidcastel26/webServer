23ffdd8141b453831bc198b1b0e4a1b9cf51ec14
David <davcastellanoslarios@gmail.com>
2021-11-09 12:18:38 -0600

    adding dir template to back up some info

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 8080;
- 
-//server a static content
-app.use( express.static('public') )
+// todo : require('hbs' ) 
+app.set('view engine', 'hbs');
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// // })
+//server a static content
+app.use( express.static('public'));
+
+app.get('/',(req, res)=> {
+  res.render('home')
+});
 // app.get('/hey-dave', (req, res) => {
 //   res.send('Hello World')
 // }) 
 
-app.get('/generic', (req, res) =>{
-  res.sendFile(__dirname + '/public/generic.html')
-})
+// app.get('/generic', (req, res) =>{
+//   res.sendFile(__dirname + '/public/generic.html')
+// })
 
 app.get('/elements', (req, res) =>{
   res.sendFile(__dirname + '/public/elements.html')
