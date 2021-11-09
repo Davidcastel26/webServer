3e767eb34999a566c50c55dc545d02ee63a81fb1
David <davcastellanoslarios@gmail.com>
2021-11-09 10:51:53 -0600

    adding tamplate

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ app.use( express.static('public') )
 
 // app.get('/', function (req, res) {
 //   res.send('Hello World')
-// })
-app.get('/hey-dave', (req, res) => {
-  res.send('Hello World')
-}) 
+// // })
+// app.get('/hey-dave', (req, res) => {
+//   res.send('Hello World')
+// }) 
+
+app.get('/generic', (req, res) =>{
+  res.sendFile(__dirname + '/public/generic.html')
+})
+
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/404.html')
 })
