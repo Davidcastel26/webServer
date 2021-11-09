1fd3b4e0a6b0fbb043a691eb9697cdb1eea19e54
David <davcastellanoslarios@gmail.com>
2021-11-09 11:44:18 -0600

    web static done

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get('/generic', (req, res) =>{
   res.sendFile(__dirname + '/public/generic.html')
 })
 
+app.get('/elements', (req, res) =>{
+  res.sendFile(__dirname + '/public/elements.html')
+})
+
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/404.html')
 })
